35fb2ceb0ad8c13f0898dd3f901e79cdd71c025a
J9273 <info@drawbridge.dk>
2026-02-12 04:12:49 +0100

    New index page

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,8 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 require_once '../includes/bootstrap.php';  // ← ADD THIS
 session_start();
 // ... rest of filerequire_once '../includes/config-render.php';
