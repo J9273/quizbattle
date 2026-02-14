3f4890fc6684a35dffae8dd7c534b4c6ce6096c2
J9273 <info@drawbridge.dk>
2026-02-14 01:50:00 +0100

    New indeximage

--- index.php
+++ index.php
@@ -8,24 +8,14 @@
 
 <body>
 	<picture>
-  <source 
-    srcset="
-      banner-mobile.webp 768w,
-      banner-laptop.webp 1440w,
-      banner-desktop.webp 1920w"
-    sizes="(max-width: 768px) 100vw,
-           (max-width: 1440px) 100vw,
-           1920px"
-    type="image/webp">
-    
+     
   <img 
     src="images/banner-desktop.webp"
     alt="Multi-user quiz app competition leaderboard"
     width="100%"
     height="auto"
     loading="eager"
-    decoding="async"
-    style="width:100%; height:auto;">
+    decoding="async">
 </picture>
 
 </body>
