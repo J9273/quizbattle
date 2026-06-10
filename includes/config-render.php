e05b01d903c035afa0184c9fa2ae621701f533ed
J9273 <96255575+J9273@users.noreply.github.com>
2026-06-10 09:07:55 +0200

    Update database configuration for production

--- includes/config-render.php
+++ includes/config-render.php
@@ -23,11 +23,11 @@ if ($database_url) {
     define('DB_TYPE', 'mysql');
 } else {
     // Local development fallback
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'postgres');
+    define('DB_HOST', 'mysql8.unoeuro.com');
+    define('DB_USER', 'drawbridge_dk');
   //  define('DB_PASS', 'SKckt79lkambNOiSbczDJovxb3rSzdvF');
-    define('DB_PASS', 'HpH1n6QgjLJg1BPJZP0xmBY2E36ugPFO');
-    define('DB_NAME', 'quizdb_f20w');
+    define('DB_PASS', '9gbnx61c94');
+    define('DB_NAME', 'drawbridge_dk');
     define('DB_PORT', 3306);
     define('DB_TYPE', 'mysql');
 }
